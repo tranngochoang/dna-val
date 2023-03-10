eba4af39b51acdeb298df8d3ba810cfbe3e25f7f
ngoc <ngoc.hoang.tran@cern.ch>
2023-03-10 15:25:02 +0100

    update dna-val

--- assets/OUTPUTJSON/geant4/MyTag/x86_64/moleculardna.c
+++ assets/OUTPUTJSON/geant4/MyTag/x86_64/moleculardna.c
@@ -6,31 +6,34 @@
 // Beam Particle:      e-
 // Secondary Particle: None
 
-double gvp_id-1_x[10] = {
+double gvp_id-1_x[11] = {
   100,
   200,
   500,
   1000,
   4500,
   10000,
+  20000,
   30000,
   100000,
   200000,
   1e+06
 };
-double gvp_id-1_y[10] = {
+double gvp_id-1_y[11] = {
   0.001,
   0.00096,
   0.00285,
   0.008,
   0.014,
   0.04144,
+  0.01731,
   0.01103,
   0.005,
   0.00257,
   0.00172
 };
-double gvp_id-1_xerrors[10] = {
+double gvp_id-1_xerrors[11] = {
+  0,
   0,
   0,
   0,
@@ -42,13 +45,14 @@ double gvp_id-1_xerrors[10] = {
   0,
   0
 };
-double gvp_id-1_yerrors[10] = {
+double gvp_id-1_yerrors[11] = {
   0.001,
   9.7933e-05,
   0.000168579,
   0.0028185,
   0.00371723,
   0.000642831,
+  0.00041654,
   0.000334192,
   0.00223159,
   0.00016073,
@@ -62,31 +66,34 @@ double gvp_id-1_yerrors[10] = {
 // Beam Particle:      e-
 // Secondary Particle: None
 
-double gvp_id-1_x[10] = {
+double gvp_id-1_x[11] = {
   100,
   200,
   500,
   1000,
   4500,
   10000,
+  20000,
   30000,
   100000,
   200000,
   1e+06
 };
-double gvp_id-1_y[10] = {
+double gvp_id-1_y[11] = {
   0.003,
   0.00103,
   0.004,
   0.00571,
   0.019,
   0.052,
+  0.02036,
   0.01219,
   0.002,
   0.00282,
   0.0016
 };
-double gvp_id-1_xerrors[10] = {
+double gvp_id-1_xerrors[11] = {
+  0,
   0,
   0,
   0,
@@ -98,237 +105,14 @@ double gvp_id-1_xerrors[10] = {
   0,
   0
 };
-double gvp_id-1_yerrors[10] = {
+double gvp_id-1_yerrors[11] = {
   0.00173032,
   0.000101437,
   0.00199699,
   0.000238274,
   0.00431945,
   0.00730406,
-  0.000349307,
-  0.00141351,
-  0.000167692,
-  0.00012639
-};
-// Tool:               GEANT4
-// Version:            MyTag
-// Model:              2
-// Observable:         moleculardna DSB
-// Target:             cylinders
-// Beam Particle:      e-
-// Secondary Particle: None
-
-double gvp_id-1_x[10] = {
-  100,
-  200,
-  500,
-  1000,
-  4500,
-  10000,
-  30000,
-  100000,
-  200000,
-  1e+06
-};
-double gvp_id-1_y[10] = {
-  0.001,
-  0.00096,
-  0.00285,
-  0.008,
-  0.014,
-  0.04144,
-  0.01103,
-  0.005,
-  0.00257,
-  0.00172
-};
-double gvp_id-1_xerrors[10] = {
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0
-};
-double gvp_id-1_yerrors[10] = {
-  0.001,
-  9.7933e-05,
-  0.000168579,
-  0.0028185,
-  0.00371723,
-  0.000642831,
-  0.000334192,
-  0.00223159,
-  0.00016073,
-  0.000131797
-};
-// Tool:               GEANT4
-// Version:            MyTag
-// Model:              4
-// Observable:         moleculardna DSB
-// Target:             cylinders
-// Beam Particle:      e-
-// Secondary Particle: None
-
-double gvp_id-1_x[10] = {
-  100,
-  200,
-  500,
-  1000,
-  4500,
-  10000,
-  30000,
-  100000,
-  200000,
-  1e+06
-};
-double gvp_id-1_y[10] = {
-  0.003,
-  0.00103,
-  0.004,
-  0.00571,
-  0.019,
-  0.052,
-  0.01219,
-  0.002,
-  0.00282,
-  0.0016
-};
-double gvp_id-1_xerrors[10] = {
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0
-};
-double gvp_id-1_yerrors[10] = {
-  0.00173032,
-  0.000101437,
-  0.00199699,
-  0.000238274,
-  0.00431945,
-  0.00730406,
-  0.000349307,
-  0.00141351,
-  0.000167692,
-  0.00012639
-};
-// Tool:               GEANT4
-// Version:            MyTag
-// Model:              2
-// Observable:         moleculardna DSB
-// Target:             cylinders
-// Beam Particle:      e-
-// Secondary Particle: None
-
-double gvp_id-1_x[10] = {
-  100,
-  200,
-  500,
-  1000,
-  4500,
-  10000,
-  30000,
-  100000,
-  200000,
-  1e+06
-};
-double gvp_id-1_y[10] = {
-  0.001,
-  0.00096,
-  0.00285,
-  0.008,
-  0.014,
-  0.04144,
-  0.01103,
-  0.005,
-  0.00257,
-  0.00172
-};
-double gvp_id-1_xerrors[10] = {
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0
-};
-double gvp_id-1_yerrors[10] = {
-  0.001,
-  9.7933e-05,
-  0.000168579,
-  0.0028185,
-  0.00371723,
-  0.000642831,
-  0.000334192,
-  0.00223159,
-  0.00016073,
-  0.000131797
-};
-// Tool:               GEANT4
-// Version:            MyTag
-// Model:              4
-// Observable:         moleculardna DSB
-// Target:             cylinders
-// Beam Particle:      e-
-// Secondary Particle: None
-
-double gvp_id-1_x[10] = {
-  100,
-  200,
-  500,
-  1000,
-  4500,
-  10000,
-  30000,
-  100000,
-  200000,
-  1e+06
-};
-double gvp_id-1_y[10] = {
-  0.003,
-  0.00103,
-  0.004,
-  0.00571,
-  0.019,
-  0.052,
-  0.01219,
-  0.002,
-  0.00282,
-  0.0016
-};
-double gvp_id-1_xerrors[10] = {
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0
-};
-double gvp_id-1_yerrors[10] = {
-  0.00173032,
-  0.000101437,
-  0.00199699,
-  0.000238274,
-  0.00431945,
-  0.00730406,
+  0.000450396,
   0.000349307,
   0.00141351,
   0.000167692,

605e486d5e61c8a999d69f18d311f02bd3e2ab39
ngoc <ngoc.hoang.tran@cern.ch>
2023-03-17 11:24:32 +0100

    ven. mars 17 11:24:31 CET 2023

---
+++ assets/OUTPUTJSON/geant4/304_11.1.0/x86_64/chem6/electron.conf.c
@@ -0,0 +1,420 @@
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              IRT_syn
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  0.464663
+};
+double gvp_id-1_y[1] = {
+  2.60717
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              IRT_syn
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  1.02565
+};
+double gvp_id-1_y[1] = {
+  2.49218
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              IRT_syn
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  0.162085
+};
+double gvp_id-1_y[1] = {
+  2.67426
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              IRT
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  1.01594
+};
+double gvp_id-1_y[1] = {
+  2.53875
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              IRT
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  3.23754
+};
+double gvp_id-1_y[1] = {
+  2.20951
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              IRT
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  9.02172
+};
+double gvp_id-1_y[1] = {
+  1.54601
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              SBS
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  8.9637
+};
+double gvp_id-1_y[1] = {
+  1.70958
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              IRT
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  2.08355
+};
+double gvp_id-1_y[1] = {
+  2.32979
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              IRT
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  0.161106
+};
+double gvp_id-1_y[1] = {
+  2.72286
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              IRT_syn
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  9.01178
+};
+double gvp_id-1_y[1] = {
+  1.53702
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              SBS
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  3.03842
+};
+double gvp_id-1_y[1] = {
+  2.38659
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              IRT_syn
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  3.09476
+};
+double gvp_id-1_y[1] = {
+  2.18791
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              SBS
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  0.16137
+};
+double gvp_id-1_y[1] = {
+  2.85617
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              IRT
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  0.465758
+};
+double gvp_id-1_y[1] = {
+  2.6488
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              SBS
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  1.00202
+};
+double gvp_id-1_y[1] = {
+  2.68463
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              SBS
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  2.07519
+};
+double gvp_id-1_y[1] = {
+  2.48608
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              IRT_syn
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  5.77984
+};
+double gvp_id-1_y[1] = {
+  1.8019
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              SBS
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  0.467616
+};
+double gvp_id-1_y[1] = {
+  2.78325
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              IRT_syn
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  2.07658
+};
+double gvp_id-1_y[1] = {
+  2.30487
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              IRT
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  5.8087
+};
+double gvp_id-1_y[1] = {
+  1.83459
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              SBS
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  5.48562
+};
+double gvp_id-1_y[1] = {
+  2.01734
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};

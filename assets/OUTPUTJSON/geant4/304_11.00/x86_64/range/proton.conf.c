f287dc7cbb21c1cb27e6ee6d97ead7c997d75a35
ngoc <ngoc.hoang.tran@cern.ch>
2023-03-20 13:39:22 +0100

    lun. mars 20 13:39:22 CET 2023

--- assets/OUTPUTJSON/geant4/304_11.00/x86_64/range/proton.conf.c
+++ assets/OUTPUTJSON/geant4/304_11.00/x86_64/range/proton.conf.c
@@ -226,3 +226,71 @@ double gvp_id-1_yerrors[13] = {
   0,
   0
 };
+// Tool:               GEANT4
+// Version:            304_11.00
+// Model:              dna_opt4
+// Observable:         microdosimetry y
+// Target:             Water
+// Beam Particle:      proton
+// Secondary Particle: None
+
+double gvp_id-1_x[13] = {
+  1000,
+  2000,
+  4000,
+  6000,
+  10000,
+  20000,
+  30000,
+  40000,
+  80000,
+  100000,
+  300000,
+  600000,
+  1e+06
+};
+double gvp_id-1_y[13] = {
+  97.244,
+  162.291,
+  259.532,
+  333.001,
+  450.491,
+  661.98,
+  824.076,
+  967.225,
+  1468.58,
+  1713.29,
+  4752.09,
+  11555,
+  24687.3
+};
+double gvp_id-1_xerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};

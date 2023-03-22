648cb845ef17d199f4f2e5f15681d1cb3c18ab93
ngoc <ngoc.hoang.tran@cern.ch>
2023-03-22 15:59:41 +0100

    icsd/electron.conf/MyTag : mer. mars 22 15:59:41 CET 2023

---
+++ assets/OUTPUTJSON/geant4/MyTag/x86_64/icsd/electron.conf.c
@@ -0,0 +1,120 @@
+// Tool:               GEANT4
+// Version:            MyTag
+// Model:              icsd
+// Observable:         mean value of icsd
+// Target:             dna
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[11] = {
+  20,
+  30,
+  50,
+  70,
+  100,
+  110,
+  200,
+  300,
+  500,
+  700,
+  1000
+};
+double gvp_id-1_y[11] = {
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333
+};
+double gvp_id-1_xerrors[11] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[11] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+// Tool:               GEANT4
+// Version:            MyTag
+// Model:              icsd
+// Observable:         mean value of icsd
+// Target:             nanodosimeter
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[11] = {
+  20,
+  30,
+  50,
+  70,
+  100,
+  110,
+  200,
+  300,
+  500,
+  700,
+  1000
+};
+double gvp_id-1_y[11] = {
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333,
+  2.21333
+};
+double gvp_id-1_xerrors[11] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[11] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};

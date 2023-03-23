b20665db657c1c994ac27d230b516804e824fd82
ngoc <ngoc.hoang.tran@cern.ch>
2023-03-23 16:48:11 +0100

    range/alpha.conf/304_11.00 : jeu. mars 23 16:48:10 CET 2023

--- assets/OUTPUTJSON/MyTag_304_11.1.0_304_11.00_range_proton.conf__dna_opt4.c
+++ assets/OUTPUTJSON/MyTag_304_11.1.0_304_11.00_range_proton.conf__dna_opt4.c
@@ -610,3 +610,207 @@ double gvp_id-1_yerrors[13] = {
   0,
   0
 };
+// Tool:               GEANT4
+// Version:            MyTag
+// Model:              dna_opt4
+// Observable:         microdosimetry y
+// Target:             Water
+// Beam Particle:      proton
+// Secondary Particle: None
+
+double gvp_id-1_x[13] = {
+  1000,
+  2000,
+  4000,
+  6000,
+  10000,
+  20000,
+  30000,
+  40000,
+  80000,
+  100000,
+  300000,
+  600000,
+  1e+06
+};
+double gvp_id-1_y[13] = {
+  97.5713,
+  162.432,
+  258.069,
+  332.877,
+  451.097,
+  661.094,
+  823.99,
+  965.786,
+  1468.51,
+  1715.95,
+  4748.44,
+  11560.4,
+  24690.1
+};
+double gvp_id-1_xerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              dna_opt4
+// Observable:         microdosimetry y
+// Target:             Water
+// Beam Particle:      proton
+// Secondary Particle: None
+
+double gvp_id-1_x[13] = {
+  1000,
+  2000,
+  4000,
+  6000,
+  10000,
+  20000,
+  30000,
+  40000,
+  80000,
+  100000,
+  300000,
+  600000,
+  1e+06
+};
+double gvp_id-1_y[13] = {
+  97.5713,
+  162.432,
+  258.069,
+  332.877,
+  451.097,
+  661.094,
+  823.99,
+  965.786,
+  1468.51,
+  1715.95,
+  4748.44,
+  11560.4,
+  24690.1
+};
+double gvp_id-1_xerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.00
+// Model:              dna_opt4
+// Observable:         microdosimetry y
+// Target:             Water
+// Beam Particle:      proton
+// Secondary Particle: None
+
+double gvp_id-1_x[13] = {
+  1000,
+  2000,
+  4000,
+  6000,
+  10000,
+  20000,
+  30000,
+  40000,
+  80000,
+  100000,
+  300000,
+  600000,
+  1e+06
+};
+double gvp_id-1_y[13] = {
+  97.244,
+  162.291,
+  259.532,
+  333.001,
+  450.491,
+  661.98,
+  824.076,
+  967.225,
+  1468.58,
+  1713.29,
+  4752.09,
+  11555,
+  24687.3
+};
+double gvp_id-1_xerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};

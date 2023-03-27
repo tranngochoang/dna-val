14ce8e8497684cac1d01c343f5b9be293a85d419
ngoc <ngoc.hoang.tran@cern.ch>
2023-03-27 12:30:43 +0200

    Finish and push to dna-val : lun. mars 27 12:30:43 CEST 2023

--- assets/OUTPUTJSON/MyTag_304_11.1.0_304_11.00_304_10.07_icsd_electron.conf__icsd.c
+++ assets/OUTPUTJSON/MyTag_304_11.1.0_304_11.00_304_10.07_icsd_electron.conf__icsd.c
@@ -1630,3 +1630,547 @@ double gvp_id-1_yerrors[13] = {
   0,
   0
 };
+// Tool:               GEANT4
+// Version:            MyTag
+// Model:              icsd
+// Observable:         mean value of icsd
+// Target:             dna
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[13] = {
+  20,
+  30,
+  50,
+  70,
+  100,
+  110,
+  200,
+  300,
+  500,
+  700,
+  800,
+  900,
+  999
+};
+double gvp_id-1_y[13] = {
+  0.3173,
+  0.7721,
+  1.53449,
+  2.24022,
+  3.16173,
+  3.39893,
+  4.06002,
+  3.2373,
+  2.04763,
+  1.51898,
+  1.35961,
+  1.22712,
+  1.12516
+};
+double gvp_id-1_xerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+// Tool:               GEANT4
+// Version:            MyTag
+// Model:              icsd
+// Observable:         mean value of icsd
+// Target:             nanodosimeter
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[13] = {
+  20,
+  30,
+  50,
+  70,
+  100,
+  110,
+  200,
+  300,
+  500,
+  700,
+  800,
+  900,
+  999
+};
+double gvp_id-1_y[13] = {
+  0.3173,
+  0.7721,
+  1.53449,
+  2.24022,
+  3.16173,
+  3.39893,
+  4.06002,
+  3.2373,
+  2.04763,
+  1.51898,
+  1.35961,
+  1.22712,
+  1.12516
+};
+double gvp_id-1_xerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              icsd
+// Observable:         mean value of icsd
+// Target:             nanodosimeter
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[13] = {
+  20,
+  30,
+  50,
+  70,
+  100,
+  110,
+  200,
+  300,
+  500,
+  700,
+  800,
+  900,
+  999
+};
+double gvp_id-1_y[13] = {
+  0.3173,
+  0.7721,
+  1.53449,
+  2.24022,
+  3.16173,
+  3.39893,
+  4.06002,
+  3.2373,
+  2.04763,
+  1.51898,
+  1.35961,
+  1.22712,
+  1.12516
+};
+double gvp_id-1_xerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.1.0
+// Model:              icsd
+// Observable:         mean value of icsd
+// Target:             dna
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[13] = {
+  20,
+  30,
+  50,
+  70,
+  100,
+  110,
+  200,
+  300,
+  500,
+  700,
+  800,
+  900,
+  999
+};
+double gvp_id-1_y[13] = {
+  0.3173,
+  0.7721,
+  1.53449,
+  2.24022,
+  3.16173,
+  3.39893,
+  4.06002,
+  3.2373,
+  2.04763,
+  1.51898,
+  1.35961,
+  1.22712,
+  1.12516
+};
+double gvp_id-1_xerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.00
+// Model:              icsd
+// Observable:         mean value of icsd
+// Target:             nanodosimeter
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[13] = {
+  20,
+  30,
+  50,
+  70,
+  100,
+  110,
+  200,
+  300,
+  500,
+  700,
+  800,
+  900,
+  999
+};
+double gvp_id-1_y[13] = {
+  0.3173,
+  0.7721,
+  1.53372,
+  2.24025,
+  3.16086,
+  3.39927,
+  4.05763,
+  3.23556,
+  2.02819,
+  1.50767,
+  1.35119,
+  1.22108,
+  1.11606
+};
+double gvp_id-1_xerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+// Tool:               GEANT4
+// Version:            304_11.00
+// Model:              icsd
+// Observable:         mean value of icsd
+// Target:             dna
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[13] = {
+  20,
+  30,
+  50,
+  70,
+  100,
+  110,
+  200,
+  300,
+  500,
+  700,
+  800,
+  900,
+  999
+};
+double gvp_id-1_y[13] = {
+  0.3173,
+  0.7721,
+  1.53372,
+  2.24025,
+  3.16086,
+  3.39927,
+  4.05763,
+  3.23556,
+  2.02819,
+  1.50767,
+  1.35119,
+  1.22108,
+  1.11606
+};
+double gvp_id-1_xerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+// Tool:               GEANT4
+// Version:            304_10.07
+// Model:              icsd
+// Observable:         mean value of icsd
+// Target:             nanodosimeter
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[13] = {
+  20,
+  30,
+  50,
+  70,
+  100,
+  110,
+  200,
+  300,
+  500,
+  700,
+  800,
+  900,
+  999
+};
+double gvp_id-1_y[13] = {
+  0.3173,
+  0.7721,
+  1.53372,
+  2.24025,
+  3.16086,
+  3.39927,
+  4.05763,
+  3.23556,
+  2.02819,
+  1.50767,
+  1.35119,
+  1.22108,
+  1.11606
+};
+double gvp_id-1_xerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+// Tool:               GEANT4
+// Version:            304_10.07
+// Model:              icsd
+// Observable:         mean value of icsd
+// Target:             dna
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[13] = {
+  20,
+  30,
+  50,
+  70,
+  100,
+  110,
+  200,
+  300,
+  500,
+  700,
+  800,
+  900,
+  999
+};
+double gvp_id-1_y[13] = {
+  0.3173,
+  0.7721,
+  1.53372,
+  2.24025,
+  3.16086,
+  3.39927,
+  4.05763,
+  3.23556,
+  2.02819,
+  1.50767,
+  1.35119,
+  1.22108,
+  1.11606
+};
+double gvp_id-1_xerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};

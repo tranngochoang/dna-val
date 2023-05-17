d79e941d890bc83dec123be9560aeb73a40d8aee
ngoc <ngoc.hoang.tran@cern.ch>
2023-05-17 17:38:07 +0200

    range/proton.conf/MyTag : mer. mai 17 17:38:07 CEST 2023

--- assets/OUTPUTJSON/geant4/MyTag/x86_64/range/proton.conf.c
+++ assets/OUTPUTJSON/geant4/MyTag/x86_64/range/proton.conf.c
@@ -678,3 +678,71 @@ double gvp_id-1_yerrors[13] = {
   0,
   0
 };
+// Tool:               GEANT4
+// Version:            MyTag
+// Model:              dna_opt4
+// Observable:         range
+// Target:             Water
+// Beam Particle:      proton
+// Secondary Particle: None
+
+double gvp_id-1_x[13] = {
+  1000,
+  2000,
+  4000,
+  6000,
+  10000,
+  20000,
+  30000,
+  40000,
+  80000,
+  100000,
+  300000,
+  600000,
+  1e+06
+};
+double gvp_id-1_y[13] = {
+  97.5933,
+  162.428,
+  258.082,
+  333.361,
+  451.142,
+  660.924,
+  823.573,
+  965.439,
+  1468.52,
+  1715.84,
+  4753.09,
+  11557.4,
+  24687.8
+};
+double gvp_id-1_xerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};

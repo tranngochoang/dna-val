039c17f54e1d3573ce49aa482dab2fcf10ae7508
ngoc <ngoc.hoang.tran@cern.ch>
2023-11-03 18:11:17 +0100

    range/proton.conf/MyTag : ven. nov.  3 18:11:16 CET 2023

--- assets/OUTPUTJSON/geant4/MyTag/x86_64/range/proton.conf.c
+++ assets/OUTPUTJSON/geant4/MyTag/x86_64/range/proton.conf.c
@@ -4486,3 +4486,139 @@ double gvp_id-1_yerrors[13] = {
   0,
   0
 };
+// Tool:               GEANT4
+// Version:            MyTag
+// Model:              dna_opt4
+// Observable:         range
+// Target:             G4_WATER
+// Beam Particle:      proton
+// Secondary Particle: None
+
+double gvp_id-1_x[13] = {
+  1000,
+  2000,
+  4000,
+  6000,
+  10000,
+  20000,
+  30000,
+  40000,
+  80000,
+  100000,
+  300000,
+  600000,
+  1e+06
+};
+double gvp_id-1_y[13] = {
+  97.5713,
+  162.432,
+  258.069,
+  332.877,
+  451.097,
+  660.954,
+  823.597,
+  965.654,
+  1468.11,
+  1715,
+  4749,
+  11560.4,
+  24690.5
+};
+double gvp_id-1_xerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+// Tool:               GEANT4
+// Version:            MyTag
+// Model:              dna_opt2
+// Observable:         range
+// Target:             G4_WATER
+// Beam Particle:      proton
+// Secondary Particle: None
+
+double gvp_id-1_x[13] = {
+  1000,
+  2000,
+  4000,
+  6000,
+  10000,
+  20000,
+  30000,
+  40000,
+  80000,
+  100000,
+  300000,
+  600000,
+  1e+06
+};
+double gvp_id-1_y[13] = {
+  97.6768,
+  161.511,
+  259.129,
+  332.394,
+  450.13,
+  661.203,
+  824.246,
+  967.244,
+  1469.28,
+  1714.63,
+  4752.15,
+  11542.1,
+  24523.7
+};
+double gvp_id-1_xerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[13] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};

3d2321222b3bd61ba79d1b0a4ba341265d93c47a
tranngochoang <tranngochoang@users.noreply.github.com>
2025-04-17 22:35:12 +0000

    Deploy 9c461f1cf5f5046b4b8c6a542d404f35bec0fe39

---
+++ assets/OUTPUTJSON/geant4/304_11.00/x86_64/chem6/electron.conf_LETOH.c
@@ -0,0 +1,132 @@
+// Tool:               LET
+// Version:            304_11.00
+// Model:              SBS
+// Observable:         OH^0
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[7] = {
+  8.9637,
+  5.743,
+  3.1572,
+  2.0826,
+  0.996068,
+  0.452977,
+  0.157492
+};
+double gvp_id-1_y[7] = {
+  1.55546,
+  1.82379,
+  2.17832,
+  2.33736,
+  2.54959,
+  2.67276,
+  2.74752
+};
+double gvp_id-1_xerrors[7] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[7] = {
+  0.0166797,
+  0.0168923,
+  0.0158593,
+  0.0139621,
+  0.012689,
+  0.0119604,
+  0.0106917
+};
+// Tool:               LET
+// Version:            304_11.00
+// Model:              IRT_syn
+// Observable:         OH^0
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[7] = {
+  9.01178,
+  5.73595,
+  3.06471,
+  2.0908,
+  0.997578,
+  0.462715,
+  0.160623
+};
+double gvp_id-1_y[7] = {
+  1.53634,
+  1.81469,
+  2.19554,
+  2.35084,
+  2.54607,
+  2.6579,
+  2.74502
+};
+double gvp_id-1_xerrors[7] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[7] = {
+  0.00920681,
+  0.00942093,
+  0.00946293,
+  0.00811207,
+  0.00701564,
+  0.00684006,
+  0.00653297
+};
+// Tool:               LET
+// Version:            304_11.00
+// Model:              IRT
+// Observable:         OH^0
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[7] = {
+  9.02172,
+  6.05585,
+  3.04345,
+  2.15027,
+  1.41065,
+  0.463729,
+  0.161745
+};
+double gvp_id-1_y[7] = {
+  1.53436,
+  1.80176,
+  2.19687,
+  2.32286,
+  2.54402,
+  2.66853,
+  2.75903
+};
+double gvp_id-1_xerrors[7] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+double gvp_id-1_yerrors[7] = {
+  0.00703122,
+  0.00750827,
+  0.00735483,
+  0.00618771,
+  0.0055866,
+  0.00544587,
+  0.00518023
+};

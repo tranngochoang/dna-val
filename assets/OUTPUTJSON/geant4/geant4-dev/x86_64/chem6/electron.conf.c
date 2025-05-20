1bbac679b984c60f4844d5b90ad0659327c7fa99
tranngochoang <tranngochoang@users.noreply.github.com>
2025-05-20 11:18:06 +0000

    Deploy 68164140c3514dca3ceb908e8225c49938f72bec

---
+++ assets/OUTPUTJSON/geant4/geant4-dev/x86_64/chem6/electron.conf.c
@@ -0,0 +1,60 @@
+// Tool:               GEANT4
+// Version:            geant4-dev
+// Model:              SBS
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  8.9637
+};
+double gvp_id-1_y[1] = {
+  1.70958
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            geant4-dev
+// Model:              IRT
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  9.02172
+};
+double gvp_id-1_y[1] = {
+  1.54678
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
+// Tool:               GEANT4
+// Version:            geant4-dev
+// Model:              IRT_syn
+// Observable:         OH* G-value in LET
+// Target:             G4_WATER
+// Beam Particle:      e-
+// Secondary Particle: None
+
+double gvp_id-1_x[1] = {
+  9.01178
+};
+double gvp_id-1_y[1] = {
+  1.53702
+};
+double gvp_id-1_xerrors[1] = {
+  0
+};
+double gvp_id-1_yerrors[1] = {
+  0
+};
